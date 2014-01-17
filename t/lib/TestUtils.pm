2d40ca47ba22faf7cd502aec25af22402b77c09e
Torbjørn Lindahl <torbjorn.lindahl@gmail.com>
2014-01-17 15:22:25 +0100

    added cleanup check

--- t/lib/TestUtils.pm
+++ t/lib/TestUtils.pm
@@ -6,12 +6,20 @@ use warnings;
 use Path::Tiny;
 
 use base 'Exporter';
-our @EXPORT = qw/clean_cat_dir test_argv goto_test_dir cat_name proj_dir/;
+our @EXPORT = qw/clean_cat_dir
+                 test_argv
+                 goto_test_dir
+                 cat_name
+                 proj_dir test_dir/;
 
 my $proj_dir = Path::Tiny->cwd;
 
 sub proj_dir {
     return $proj_dir;
+}
+
+sub test_dir {
+    return path( proj_dir, "t/lib/testdir", @_ );
 }
 
 sub cat_name {
@@ -24,9 +32,15 @@ sub test_argv {
 }
 
 sub goto_test_dir {
-    chdir path( $proj_dir, "t/lib/testdir");
+    chdir path( test_dir );
 }
 
 sub clean_cat_dir {
-    $_->remove_tree for path( $proj_dir, "t/lib/testdir")->children;
+    $_->remove_tree for test_dir->children;
+}
+
+END {
+    if ( test_dir->children ) {
+        warn path($0)->basename, ": I did not clean up testdir after me, I am a bad script\n";
+    }
 }
